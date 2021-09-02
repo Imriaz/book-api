f2b9083d571fdd90fd1c7a78fb963006d55130b5
Md. Imriaz Uddin <imriaz.cse.bubt@gmail.com>
2021-09-02 12:31:25 +0600

    API Problem fixed at  live site

--- js/book.js
+++ js/book.js
@@ -22,7 +22,7 @@ searchBtn.addEventListener('click', function () {
     errorDiv.innerHTML = '';
 
     //Fetch the API
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
 
     fetch(url)
         .then(res => res.json())
